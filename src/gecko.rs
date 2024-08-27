0a910ae0cb0103c836cc6277f1f530b57df026d2
Naveen-Lab <naveenbraphael@gmail.com>
2024-08-27 22:13:52 +0530

    changed to fantocinni, almost

--- src/gecko.rs
+++ src/gecko.rs
@@ -1,18 +1,28 @@
 use std::process::Command;
 
-pub struct Gecko(std::process::Child);
-impl Gecko {
-    pub fn new() -> Result<Self, String> {
-        match Command::new("geckodriver")
+pub enum DriverTypes {
+    Gecko,
+    Edge,
+}
+
+pub struct DriverSpawn(std::process::Child);
+impl DriverSpawn {
+    pub fn new(dr: DriverTypes) -> Result<Self, String> {
+        match dr {
+            DriverTypes::Gecko => {
+                match Command::new("geckodriver")
             .args(["--log", "fatal"])
             .spawn(){
             Err(e) =>Err(format!("Geckodriver possibly not installed; please run `cargo install geckodriver`. Actual error: {}", e)),
-            Ok(a)=> Ok(Gecko(a)),
+            Ok(a)=> Ok(DriverSpawn(a)),
+        }
+            }
+            DriverTypes::Edge => todo!(),
         }
     }
 }
 
-impl Drop for Gecko {
+impl Drop for DriverSpawn {
     fn drop(&mut self) {
         self.0.kill().expect("This should work, you know");
     }
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,54 @@
 mod gecko;
 mod login;
+use fantoccini::{Client, ClientBuilder, Locator};
+use gecko::{DriverSpawn, DriverTypes};
+use login::{Login, LoginTypes};
 use std::{thread, time::Duration};
 
-use gecko::Gecko;
-use login::{Login, LoginTypes};
-use thirtyfour::prelude::{By, DesiredCapabilities, WebDriver, WebDriverError, WebDriverResult};
-
-async fn navigate_site(login: Login, driver: &WebDriver) -> WebDriverResult<()> {
+async fn navigate_site(login: Login, driver: &Client) -> Result<(), fantoccini::error::CmdError> {
     driver.goto("https://netaccess.iitm.ac.in").await?;
 
-    let username_fill = driver.find(By::Id("username")).await?;
-    username_fill.send_keys(login.username()).await?;
+    let user_name_fill = driver.wait().for_element(Locator::Id("username")).await?;
+    user_name_fill.send_keys(login.username().as_str()).await?;
 
-    let password_fill = driver.find(By::Id("password")).await?;
-    password_fill.send_keys(login.password()).await?;
+    let password_fill = driver.wait().for_element(Locator::Id("password")).await?;
+    password_fill.send_keys(login.password().as_str()).await?;
 
-    let first_button = driver.find(By::Id("submit")).await?;
+    let first_button = driver.wait().for_element(Locator::Id("submit")).await?;
     first_button.click().await?;
 
     driver
         .goto("https://netaccess.iitm.ac.in/account/approve")
         .await?;
-    let select_time = driver.find(By::Id("radios-1")).await?;
+    let select_time = driver.wait().for_element(Locator::Id("radios-1")).await?;
     select_time.click().await?;
 
-    let second_button = driver.find(By::Id("approveBtn")).await?;
+    let second_button = driver.wait().for_element(Locator::Id("approveBtn")).await?;
     second_button.click().await?;
 
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> WebDriverResult<()> {
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
     let login = Login::new(LoginTypes::Environment);
 
-    let _gecko = match Gecko::new() {
+    let _gecko = match DriverSpawn::new(DriverTypes::Gecko) {
         Ok(g) => g,
         Err(a) => panic!("{}", a),
     };
 
     //Because Linux is just that fast smh
-    let driver = match WebDriver::new("http://localhost:4444", DesiredCapabilities::firefox()).await
+    let driver = match ClientBuilder::native()
+        .connect("http://localhost:4444")
+        .await
     {
         Ok(a) => a,
         Err(_e) => {
             thread::sleep(Duration::from_millis(10));
-            if let Ok(a) =
-                WebDriver::new("http://localhost:4444", DesiredCapabilities::firefox()).await
+            if let Ok(a) = ClientBuilder::native()
+                .connect("http://localhost:4444")
+                .await
             {
                 a
             } else {
@@ -55,9 +57,13 @@ async fn main() -> WebDriverResult<()> {
         }
     };
 
-    navigate_site(login, &driver).await?;
+    let Ok(_) = navigate_site(login, &driver).await else {
+        println!("Error in navigating site");
+        return;
+    };
 
-    driver.quit().await?;
-
-    Ok(())
+    let Ok(_) = driver.close().await else {
+        println!("Error in closing driver");
+        return;
+    };
 }
